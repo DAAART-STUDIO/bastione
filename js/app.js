38118357fe386744bb67c2f0c6d9cbc1a296f2cc
daaart <daaart.solutions@gmail.com>
2026-08-19 19:46:36 +0300

    feat: meke view section and redesign view gallery

--- js/app.js
+++ js/app.js
@@ -6,10 +6,12 @@ import { initThemeSwitcher } from './modules/theme-switcher.js';
 import { initNavigation } from './modules/navigation.js';
 import { initI18n } from './modules/i18n.js';
 import { initHeader } from './modules/header.js';
+import { initViewGallery } from './modules/view-gallery.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initThemeSwitcher();
   initNavigation();
   initI18n();
   initHeader();
+  initViewGallery();
 });
